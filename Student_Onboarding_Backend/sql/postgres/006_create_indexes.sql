95db5490def6d1701ce069261c756cf8f7b35d60
Suchi684 <itssuchi1202@gmail.com>
2026-04-07 17:32:49 +0530

    Postgres query updated

--- Student_Onboarding_Backend/sql/postgres/006_create_indexes.sql
+++ Student_Onboarding_Backend/sql/postgres/006_create_indexes.sql
@@ -7,7 +7,6 @@ CREATE INDEX IF NOT EXISTS idx_users_email ON Users(Email);
 CREATE INDEX IF NOT EXISTS idx_users_phone ON Users(PhoneNumber);
 
 CREATE INDEX IF NOT EXISTS idx_sessions_userid ON UserSessions(UserId);
-CREATE INDEX IF NOT EXISTS idx_sessions_refreshtoken ON UserSessions(RefreshToken);
 
 CREATE INDEX IF NOT EXISTS idx_otp_email ON OtpVerifications(Email);
 CREATE INDEX IF NOT EXISTS idx_otp_phone ON OtpVerifications(PhoneNumber);
